056489f61d1e14407d70e00d7051101ee610bd01
shrabro <shrabro1@in.ibm.com>
2019-07-17 17:20:34 +0530

    first commit

--- src/main/java/com/stackroute/keepnote/repository/NoteRepository.java
+++ src/main/java/com/stackroute/keepnote/repository/NoteRepository.java
@@ -1,11 +1,15 @@
 package com.stackroute.keepnote.repository;
 
+import java.util.ArrayList;
 import java.util.List;
+
+
 
 import com.stackroute.keepnote.model.Note;
 
+
 /*
- * This class contains the code for data storage interactions and methods 
+ * This class contains the code for data storage interactions and methods
  * of this class will be used by other parts of the applications such
  * as Controllers and Test Cases
  * */
@@ -13,22 +17,23 @@ import com.stackroute.keepnote.model.Note;
 public class NoteRepository {
 
 	/* Declare a variable called "list" to store all the notes. */
+	List<Note> list;
 
 	public NoteRepository() {
+		list =new ArrayList<Note>();
 
 		/* Initialize the variable using proper data type */
 	}
 
 	/* This method should return all the notes in the list */
-
 	public List<Note> getList() {
-		return null;
+		return list;
 	}
 
 	/* This method should set the list variable with new list of notes */
 
 	public void setList(List<Note> list) {
-
+		this.list = list;
 	}
 
 	/*
@@ -37,22 +42,28 @@ public class NoteRepository {
 	 */
 
 	public void addNote(Note note) {
-
+		list.add(0, note);
 	}
 
 	/* This method should deleted a specified note from the list */
 
 	public boolean deleteNote(int noteId) {
 		/* Use list iterator to find matching note id and remove it from the list */
+		for(int i=0;i<list.size();i++) {
+			if(list.get(i).getNoteId()==noteId) {
+				list.remove(list.get(i));
+				return true;
+			}
+		}
 		return false;
-		
-		
+
+
 	}
 
 	/* This method should return the list of notes */
 
 	public List<Note> getAllNotes() {
-		return null;
+		return list;
 	}
 
 	/*
@@ -62,6 +73,13 @@ public class NoteRepository {
 	 */
 
 	public boolean exists(int noteId) {
-		return false;
+		boolean flag=false;
+		for(int i=0;i<list.size();i++) {
+			if(list.get(i).getNoteId()==noteId) {
+				flag = true;
+				break;
+			}
+		}
+		return flag;
 	}
 }
