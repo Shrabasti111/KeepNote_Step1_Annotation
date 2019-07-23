e90748b5719253d7712692fcf44bec37639bac3f
shrabro <shrabro1@in.ibm.com>
2019-07-23 10:33:12 +0530

    Annotations edit

--- src/main/java/com/stackroute/keepnote/controller/NoteController.java
+++ src/main/java/com/stackroute/keepnote/controller/NoteController.java
@@ -1,27 +1,27 @@
 package com.stackroute.keepnote.controller;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import com.stackroute.keepnote.MyConfig;
 import com.stackroute.keepnote.model.Note;
 import com.stackroute.keepnote.repository.NoteRepository;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.time.LocalDateTime;
 
 /*Annotate the class with @Controller annotation. @Controller annotation is used to mark
  * any POJO class as a controller so that Spring can recognize this class as a Controller
  * */
 @Controller
 public class NoteController {
+
+	private NoteRepository noteRepository = getRepository();
 
 	/*
 	 * From the problem statement, we can understand that the application
@@ -34,25 +34,50 @@ public class NoteController {
 	 * 4. Update an existing note.
 	 */
 
+	public void setNoteRepository(NoteRepository noteRepository) {
+		this.noteRepository = noteRepository;
+	}
+
+
+	public NoteRepository getNoteRepository() {
+		return noteRepository;
+	}
+	private AnnotationConfigApplicationContext appContext;
+
+
+
 	/*
 	 * Get the application context from resources/beans.xml file using ClassPathXmlApplicationContext() class.
 	 * Retrieve the Note object from the context.
 	 * Retrieve the NoteRepository object from the context.
 	 */
-	ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-	Note note = (Note) context.getBean("note");
-	NoteRepository noteRepository = (NoteRepository) context.getBean("noteRepository");
+	private ApplicationContext getAppContext() {
+		return new AnnotationConfigApplicationContext(MyConfig.class);
+	}
+
+	private NoteRepository getRepository() {
+		ApplicationContext appContext = getAppContext();
+		return appContext.getBean("noteRepository", NoteRepository.class);
+	}
+	private Note getNote() {
+		ApplicationContext appContext = getAppContext();
+		return appContext.getBean("note", Note.class);
+	}
+
+
 	/*Define a handler method to read the existing notes by calling the getAllNotes() method
 	 * of the NoteRepository class and add it to the ModelMap which is an implementation of Map
 	 * for use when building model data for use with views. it should map to the default URL i.e. "/" */
-
-	@RequestMapping("/")
-	public String addNote(ModelMap model) {
-		List<Note> noteList = new ArrayList<Note>();
-		noteList = noteRepository.getAllNotes();
-		model.addAttribute("SavedNotes",noteList);
-		return "index";
+	@GetMapping("/")
+	public ModelAndView getAllNotes() {
+		ModelAndView model = new ModelAndView();
+		model.addObject("notes", noteRepository.getAllNotes());
+		model.setViewName("index");
+		return model;
 	}
+
+
+
 	/*Define a handler method which will read the Note data from request parameters and
 	 * save the note by calling the addNote() method of NoteRepository class. Please note
 	 * that the createdAt field should always be auto populated with system time and should not be accepted
@@ -61,34 +86,45 @@ public class NoteController {
 	 * should be sent back to the view using ModelMap.
 	 * This handler method should map to the URL "/saveNote".
 	 */
-	@RequestMapping("/saveNote")
-	public String saveNote(ModelMap model ,@RequestParam Integer noteId,@RequestParam String noteTitle,@RequestParam String noteContent,@RequestParam String noteStatus ) {
-		if(noteId==null||noteTitle.isEmpty()||noteContent.isEmpty()||noteStatus.isEmpty()) {
-			model.addAttribute("error","Please fill out the required fields");
-		}else if(noteRepository.exists(noteId)) {
-			model.addAttribute("error","Note ID already exists");
-		}else {
-			Note note1 = (Note) context.getBean("note");
-			note1.setCreatedAt(LocalDateTime.now());
-			note1.setNoteId(noteId);
-			note1.setNoteTitle(noteTitle);
-			note1.setNoteContent(noteContent);
-			note1.setNoteStatus(noteStatus);
-			noteRepository.addNote(note1);
-//			List<Note> savedList = noteRepository.getAllNotes();
+	@PostMapping("/saveNote")
+	public ModelAndView addNote(@RequestParam Integer noteId,
+								@RequestParam String noteTitle, @RequestParam String noteContent,
+								@RequestParam String noteStatus) {
+
+		ModelAndView model = new ModelAndView();
+		if(noteTitle.isEmpty()||noteTitle.isEmpty()||noteTitle.isEmpty()||noteId==null) {
+			model.addObject("error","Please fill out the required fields");}
+		else if(noteRepository.exists(noteId)) {
+			model.addObject("error","Note ID already exists");
 		}
-		model.addAttribute("SavedNotes",noteRepository.getList());
-		return "index";
+
+		else
+		{ Note note = getNote();
+			note.setNoteId(noteId);
+			note.setNoteTitle(noteTitle);
+			note.setNoteContent(noteContent);
+			note.setNoteStatus(noteStatus);
+			note.setCreatedAt(LocalDateTime.now());
+			noteRepository.addNote(note);
+
+			model.addObject("newNote", note);
+		}
+		model.addObject("notes", noteRepository.getAllNotes());
+		model.setViewName("index");
+		return model;
 	}
 
 	/* Define a handler method to delete an existing note by calling the deleteNote() method
 	 * of the NoteRepository class
 	 * This handler method should map to the URL "/deleteNote"
 	 */
-	@RequestMapping("/deleteNote")
-	public String deleteNote(ModelMap model ,@RequestParam int noteId) {
+	@GetMapping("/deleteNote")
+	public ModelAndView deleteNote(@RequestParam int noteId) {
 		noteRepository.deleteNote(noteId);
-		model.addAttribute("SavedNotes", noteRepository.getAllNotes());
-		return "redirect:"+"/";
+		ModelAndView model = new ModelAndView("redirect:/");
+		model.addObject("notes", noteRepository.getAllNotes());
+		return model;
+
+
 	}
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8" isELIgnored="false"%>
-	<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+   pageEncoding="UTF-8" isELIgnored="false"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -9,71 +9,37 @@
 <title>KeepNote</title>
 </head>
 <body>
-	<!-- Create a form which will have text boxes for Note ID, title, content and status along with a Send
-		 button. Handle errors like empty fields -->
-	<h2>Note Information</h2>
+   <form action="saveNote" method="POST">
+      Note Id:<br> <input type="text" name="noteId"> <br>
+      Title:<br> <input type="text" name="noteTitle"> <br>
+      Content:<br> <input type="text" name="noteContent"> <br>
+      Status:<br> <input type="text" name="noteStatus"> <br> <br>
+      <input type="submit" value="Submit">
+   </form>
 
-	<form action="saveNote" method="post">
+   <!-- Create a form which will have text boxes for Note ID, title, content and status along with a Send
+       button. Handle errors like empty fields -->
 
-	  Note ID:<br>
-	  <input type="number" id="noteId" name="noteId"><br>
-	  Title:<br>
-	  <input type="text" id="noteTitle" name="noteTitle"><br>
-	  Content:<br>
-	  <input type="textarea" id="noteContent" name="noteContent"><br>
-	  Status:<br>
-	  <select id="noteStatus" name= "noteStatus">
-		  <option value="active">Active</option>
-		  <option value="inactive">Inactive</option>
-	  </select>
-	  <br>
-	  <br>
-	  <input type="submit" value="Submit">
 
-	</form>
-	<br>
-	<!-- display all existing notes in a tabular structure with Id, Title,Content,Status, Created Date and Action -->
-	<c:if test="${not empty error}">
-	   <p style="color:red">Error</p>: ${error}
-	</c:if>
-	<h2>Saved Notes</h2>
-	<table>
-	<c:forEach items="${SavedNotes}" var="note">
-		<tr>
-		<td>Note ID</td>
-		<td>${note.noteId}</td>
-		</tr>
+   <!-- display all existing notes in a tabular structure with Id, Title,Content,Status, Created Date and Action -->
+      <table>
+           <tbody>
+           <c:forEach var="note" items="${notes}" varStatus="status">
+           <tr>
+           <td>${note.noteId}</td><br>
+           <td>${note.noteTitle}</td><br>
+           <td>${note.noteContent}</td><br>
+           <td>${note.noteStatus}</td><br>
+           <td>${note.createdAt}</td>
+           <td><form action="deleteNote" method="GET"><input type="hidden" name="noteId" value="${note.noteId}"><input type="submit" value="Delete"></form>
+           </td>
+           </tr>
 
-		<tr>
-		<td>Title</td>
-		<td>${note.noteTitle}</td>
-		</tr>
+           </c:forEach>
+           </tbody>
+           </table>
 
-		<tr>
-		<td>Content</td>
-		<td>${note.noteContent}</td>
-		</tr>
-
-		<tr>
-		<td>Status</td>
-		<td>${note.noteStatus}</td>
-		</tr>
-
-		<tr>
-		<td>Created at</td>
-		<td>${note.createdAt}</td>
-		</tr>
-		<tr>
-		<td>
-
-		<form action="deleteNote" method="post">
-			<input type="hidden" id="noteId" name="noteId" value="${note.noteId}"/>
-			<button type="submit">Delete</button>
-		</form>
-		</td>
-		</tr>
-		</c:forEach>
-	</table>
+       <h1>${error}<h1>
 
 </body>
 </html>
